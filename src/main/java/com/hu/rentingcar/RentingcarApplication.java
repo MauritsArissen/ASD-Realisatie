1d73171ffbcc284d204a6d38788d4668d200a44d
Maurits Arissen <maurits.arissen@student.hu.nl>
2020-09-28 14:59:28 +0200

    Initialize spring application

---
+++ src/main/java/com/hu/rentingcar/RentingcarApplication.java
@@ -0,0 +1,14 @@
+package com.hu.rentingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentingcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentingcarApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/hu/rentingcar/RentingcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu.rentingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentingcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
